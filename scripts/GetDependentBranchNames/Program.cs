45d8d02b35ffcf3dff8ea78907be026dea1670b6
Jared Dixey-Hefty <jared@improbable.io>
2019-08-14 14:01:35 +0100

    Trigger dependent buildkite pipelines
    * Fixed error checking in `.ps1` files
    * Removed the `spatial` script hack for buildkite on Linux

---
+++ scripts/GetDependentBranchNames/Program.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Medallion.Shell;
+
+namespace BuildNugetPackages
+{
+
+    internal class Program
+    {
+        private static readonly Shell shell = new Shell(options => options.ThrowOnError());
+
+        private struct Dependency
+        {
+            public string Label { get; set; }
+            public string Repo { get; set; }
+            public string Trigger { get; set; }
+        }
+
+        private static readonly List<Dependency> Dependencies = new List<Dependency>
+        {
+            new Dependency
+            {
+                Label = "build-database-sync-worker",
+                Trigger = "database-sync-worker-premerge",
+                Repo = "https://github.com/spatialos/database-sync-worker.git"
+            }
+        };
+
+        private static int Main(string[] args)
+        {
+            try
+            {
+                var currentBranch = Environment.GetEnvironmentVariable("BUILDKITE_BRANCH");
+
+                if (string.IsNullOrEmpty(currentBranch))
+                {
+                    throw new Exception("The BUILDKITE_BRANCH environment variable is empty or not set.");
+                }
+
+                foreach (var dep in Dependencies)
+                {
+                    var lines = new List<string>();
+                    shell.Run("git", "ls-remote", "--heads", dep.Repo, currentBranch)
+                        .RedirectTo(lines)
+                        .RedirectStandardErrorTo(Console.Error)
+                        .Wait();
+
+                    var remoteBranch = lines.Any() && lines.First().Contains(currentBranch) ? currentBranch : "master";
+
+                    Console.Out.WriteLine($@"steps:
+  - label: ""{dep.Label}""
+    trigger: {dep.Trigger}
+    build:
+      branch: {remoteBranch}
+      env:
+        CSHARP_TEMPLATE_BRANCH: ""{currentBranch}""");
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
