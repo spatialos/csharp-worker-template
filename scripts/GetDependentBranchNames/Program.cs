83683e6e6a042f0202714e8ef752d3aa54bfab65
Jared Hefty <jared@improbable.io>
2019-08-14 10:53:03 +0100

    Switch to using Medallion.Shell

--- scripts/GetDependentBranchNames/Program.cs
+++ scripts/GetDependentBranchNames/Program.cs
@@ -1,21 +1,25 @@
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Medallion.Shell;
 
 namespace BuildNugetPackages
 {
     internal class Program
     {
+        private static readonly Shell shell = new Shell(options => options.ThrowOnError());
+
         private static int Main(string[] args)
         {
             try
             {
                 // Default to "master", unless the downstream dependency has a branch matching the same name, then use that.
-                var currentBranch = Environment.GetEnvironmentVariable("BUILDKITE_BRANCH");
-                var checkRemoteBranch = RunRedirected("git", "ls-remote", "--heads", "https://github.com/spatialos/database-sync-worker.git", currentBranch).Trim();
+                var currentBranch = Environment.GetEnvironmentVariable("BUILDKITE_BRANCH") ?? "master";
 
-                var remoteBranch = checkRemoteBranch.Contains(currentBranch) ? currentBranch : "master";
+                var lines = new List<string>();
+                shell.Run("git", "ls-remote", "--heads", "https://github.com/spatialos/database-sync-worker.git", currentBranch).RedirectTo(lines).Wait();
+
+                var remoteBranch = lines.Any() && lines.First().Contains(currentBranch) ? currentBranch : "master";
 
                 Console.Out.WriteLine($@"steps:
   - label: ""build-database-sync-worker""
@@ -31,52 +35,6 @@ namespace BuildNugetPackages
                 return 1;
             }
             return 0;
-        }
-
-        private static string RunRedirected(string command, params string[] args)
-        {
-            var info = new ProcessStartInfo(command, string.Join(' ', args))
-            {
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardError = true,
-                RedirectStandardOutput = true
-            };
-
-            using (var process = Process.Start(info))
-            {
-                if (process == null) throw new Exception($"Failed to start {command}");
-
-                var output = new StringBuilder();
-                process.OutputDataReceived += (sender, eventArgs) =>
-                {
-                    if (!string.IsNullOrEmpty(eventArgs.Data))
-                    {
-                        output.AppendLine(eventArgs.Data);
-                    }
-                };
-
-                process.ErrorDataReceived += (sender, eventArgs) =>
-                {
-                    if (!string.IsNullOrEmpty(eventArgs.Data))
-                    {
-                        Console.Error.WriteLine(eventArgs.Data);
-                    }
-                };
-
-                process.EnableRaisingEvents = true;
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-
-                process.WaitForExit();
-
-                if (process.ExitCode != 0)
-                {
-                    throw new Exception($"Non-zero exit code: {command} {string.Join(" ", args)}");
-                }
-
-                return output.ToString();
-            }
         }
     }
 }
