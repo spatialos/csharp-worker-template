cf4c5f349746a8c1af2c67190c4afd32b51c61e6
Jared Dixey-Hefty <jared@improbable.io>
2020-02-06 13:19:55 +0000

    Artifact fixes

--- scripts/build-nuget-packages.sh
+++ scripts/build-nuget-packages.sh
@@ -3,8 +3,6 @@ set -e -u -x -o pipefail
 
 cd "$(dirname "$0")/../"
 
-mkdir -p ./nupkgs
-
 # For simplicity, some packages depend on Improbable.WorkerSdkInterop. Make sure that's packaged first.
 dotnet pack Improbable/Improbable.WorkerSdkInterop.sln -p:Platform=x64 --output "$(pwd)/nupkgs"
 dotnet pack Improbable/Improbable.sln -p:Platform=x64 --output "$(pwd)/nupkgs"
