ff5eb26f13818be72fe8de44404c1e8805ef1d34
Jared Dixey-Hefty <jared@improbable.io>
2020-02-06 09:49:44 +0000

    Align CSharpCodeGenerator Fixed windows build

--- scripts/build-nuget-packages.ps1
+++ scripts/build-nuget-packages.ps1
@@ -4,19 +4,14 @@ Set-Location "$PSScriptRoot/../"
 
 $OutputDir=(Get-Location)
 
-if (Test-Path "$env:USERPROFILE/.nuget/packages") {
-    Remove-Item -Recurse -Force -Path "$env:USERPROFILE/.nuget/packages/improbable.*" | Out-Null
-}
-
 New-Item -ItemType Directory -Force -Path "$OutputDir/nupkgs" | Out-Null
 
-# For simplicity, some packages depend on Improbable.WorkerSdkInterop. Make sure that's packaged first.
-& dotnet pack Improbable/WorkerSdkInterop/Improbable.WorkerSdkInterop -p:Platform=x64 --output "$OutputDir/nupkgs"
+& dotnet pack Improbable/Improbable.WorkerSdkInterop.sln -p:Platform=x64 --output "$OutputDir/nupkgs"
 if (!$?) {
     exit 1
 }
 
-& dotnet pack Improbable -p:Platform=x64 --output "$OutputDir/nupkgs"
+& dotnet pack Improbable/Improbable.sln -p:Platform=x64 --output "$OutputDir/nupkgs"
 if (!$?) {
     exit 1
 }
